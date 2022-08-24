35ef5902ca81e0f239e95a6d71fe2f1b8975d103
Mercy-Kanyi <wakonyocasey@gmail.com>
2022-08-24 20:07:34 +0000

    betty checks

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,8 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right -
- *
+ * binary_tree_insert_right - function name
+ * @parent: binary t
+ * @value: something
  * Return: whatever
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)

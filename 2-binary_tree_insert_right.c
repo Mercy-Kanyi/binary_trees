df0997004135f246e09fb74e4435ee824f284a5f
mukhtarbdirahman <mukhtarabdirahmanm@gmail.com>
2022-08-24 22:46:13 +0300

    fixing bug

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -2,7 +2,8 @@
 
 /**
  * binary_tree_insert_right -
- *
+ *@parent: pointer to parent
+ *@value: value
  * Return: whatever
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)

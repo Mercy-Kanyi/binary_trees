ad68703c7c3f4cabd8210c2ce5e4e2b32ec362da
mukhtarbdirahman <mukhtarabdirahmanm@gmail.com>
2022-08-25 09:48:03 +0300

    12 binary

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,16 +1,18 @@
+#include <stdlib.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves - counts the leaves in a tree
- *@tree: pointer to the tree
- * Return: number of leaves
- */
+* binary_tree_leaves - pre-order traversal of a binary tree
+* @tree: pointer to the root node
+* Return: size_t
+*/
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-	else
-		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+if (!tree)
+return (0);
+
+if (tree->left == NULL && tree->right == NULL)
+return (1);
+
+return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
